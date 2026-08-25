b0c5aa13d9145a32f618a080443a338acb18990d
Kyle Lee <kyle.lee99@kakaocorp.com>
2026-08-26 07:18:33 +0900

    데이터 갱신 2026-08-26 07:18

--- data/articles.js
+++ data/articles.js
@@ -1,10 +1,10 @@
 window.KMW_DATA = {
- "generatedAt": "2026-08-26T07:08:15+09:00",
- "runCount": 582,
+ "generatedAt": "2026-08-26T07:18:32+09:00",
+ "runCount": 583,
  "price": {
   "price": "36,050",
   "pct": "+0.70",
-  "at": "2026-08-26T07:08:15+09:00"
+  "at": "2026-08-26T07:18:32+09:00"
  },
  "articles": [
   {
@@ -18237,6 +18237,27 @@ window.KMW_DATA = {
     "neg": []
    },
    "firstSeenAt": "2026-08-26T04:32:33+09:00"
+  },
+  {
+   "id": "87e1d38aac2d117b",
+   "title": "조세금융신문",
+   "outlet": "조세금융신문",
+   "outletGroup": "기타",
+   "url": "https://news.google.com/rss/articles/CBMiZkFVX3lxTE0xcXl4QndJaWE3SUdidDhWcVU2dlJCTXp1T0lKMG0wbW5aSTJ6Snl4cm01clJmMjZpbVhCc1hrSUx1SmVLWkc1NGF3SnVtSy03S19SSVJMWTZHUVJDSDRMUGpjY3NGQQ?oc=5",
+   "publishedAt": "2026-08-21T13:34:44+09:00",
+   "tone": "중립",
+   "toneScore": 0.0,
+   "frame": "중립전달",
+   "messages": [],
+   "signals": [],
+   "frames": [
+    "기타"
+   ],
+   "hits": {
+    "pos": [],
+    "neg": []
+   },
+   "firstSeenAt": "2026-08-26T07:18:32+09:00"
   }
  ]
 };
