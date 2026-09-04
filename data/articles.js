bdf15813576944d536c684c5ade075c38877253a
Kyle Lee <kyle.lee99@kakaocorp.com>
2026-09-04 14:53:13 +0900

    데이터 갱신 2026-09-04 14:53

--- data/articles.js
+++ data/articles.js
@@ -1,10 +1,10 @@
 window.KMW_DATA = {
- "generatedAt": "2026-09-04T14:42:48+09:00",
- "runCount": 829,
+ "generatedAt": "2026-09-04T14:53:09+09:00",
+ "runCount": 830,
  "price": {
-  "price": "36,150",
-  "pct": "+2.26",
-  "at": "2026-09-04T14:42:48+09:00"
+  "price": "36,100",
+  "pct": "+2.12",
+  "at": "2026-09-04T14:53:10+09:00"
  },
  "articles": [
   {
@@ -25643,6 +25643,27 @@ window.KMW_DATA = {
     ]
    },
    "firstSeenAt": "2026-09-04T13:18:08+09:00"
+  },
+  {
+   "id": "40eefb37ed81899d",
+   "title": "카카오 새 판짜는 신종환…NH 택한 까닭 : 네이버 블로그",
+   "outlet": "Naver Blog",
+   "outletGroup": "기타",
+   "url": "https://news.google.com/rss/articles/CBMijwFBVV95cUxNRGVCVHRKcFRxQlI5VWljZ056U0xfRkM3TU5TNVcwc0pnN2pCN09RQjNuRXB3cW41Y0xBbDlfZ2JJNk03dEdaUF9LZm9wVTBoY3I3aVUwUndpX3EzSjdnaGE4M3lEbUF4TElJbTFOZVEtdThjX21YTEx4bXllY0ZYcE14UXFMN1lmdVZiTHVrSQ?oc=5",
+   "publishedAt": "2026-09-03T08:50:33+09:00",
+   "tone": "중립",
+   "toneScore": 0.0,
+   "frame": "중립전달",
+   "messages": [],
+   "signals": [],
+   "frames": [
+    "기타"
+   ],
+   "hits": {
+    "pos": [],
+    "neg": []
+   },
+   "firstSeenAt": "2026-09-04T14:53:09+09:00"
   }
  ]
 };
